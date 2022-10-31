f8342c0ac8ae7be7a856d196dc8af4b9533bcb51
Muhammad Abbas <abbas@Muhammads-MacBook-Pro.local>
2022-10-31 13:34:50 +0400

    space between imports

--- src/main/java/com/esafzay/sample/domainone/PeopleService.java
+++ src/main/java/com/esafzay/sample/domainone/PeopleService.java
@@ -3,6 +3,7 @@ package com.esafzay.sample.domainone;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
