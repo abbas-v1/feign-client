03650b58f8bdf0c27e422a4e47fcaf0f4c3e4826
Muhammad Abbas <abbas@Muhammads-MacBook-Pro.local>
2022-12-23 18:54:06 +0400

    space in imports

--- src/main/java/com/esafzay/sample/domainone/PeopleController.java
+++ src/main/java/com/esafzay/sample/domainone/PeopleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
