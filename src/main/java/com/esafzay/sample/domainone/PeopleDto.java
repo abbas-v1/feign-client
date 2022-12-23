a26150c4a80d30f1c2350ae13ed2220060428305
Muhammad Abbas <abbas@Muhammads-MacBook-Pro.local>
2022-12-23 20:03:36 +0400

    unneeded space

--- src/main/java/com/esafzay/sample/domainone/PeopleDto.java
+++ src/main/java/com/esafzay/sample/domainone/PeopleDto.java
@@ -3,4 +3,4 @@ package com.esafzay.sample.domainone;
 import java.util.UUID;
 
 
-public record PeopleDto(UUID id, String name, String gender) {}
+public record PeopleDto(UUID id,   String name, String gender) {}
